0c1e0ec547ca28076aaadfc7cf8c96175d755e19
Dzmitry Liavonenka <dzmitryliavonenka@gmail.com>
2022-10-17 23:01:33 +0300

    feat:  - added main CRUD operation for todolist and task

--- src/stories/todolists-api.stories.tsx
+++ src/stories/todolists-api.stories.tsx
@@ -11,6 +11,7 @@ export const GetTodolists = () => {
     useEffect(() => {
         todolistAPI.getTodolists()
             .then((res) => {
+                debugger
                 setState(res.data)
             })
 
@@ -55,3 +56,64 @@ export const UpdateTodolistTitle = () => {
     return <div>{JSON.stringify(state)}</div>
 }
 
+export const GetTasks = () => {
+    const [state, setState] = useState<any>(null)
+    useEffect(() => {
+        const todolistId = '76332976-7428-4af5-8ba5-b86e3d1c64af'
+        todolistAPI.getTasks(todolistId)
+            .then((res) => {
+                setState(res.data.items)
+            })
+
+    }, [])
+    return <div>{JSON.stringify(state)}</div>
+}
+export const CreateTask = () => {
+    const [state, setState] = useState<any>(null)
+
+    useEffect(() => {
+        const todolistId = '76332976-7428-4af5-8ba5-b86e3d1c64af'
+        todolistAPI.createTask(todolistId, "TASK 4")
+            .then((res)=> {
+                setState(res.data)
+            })
+
+    }, [])
+
+    return <div>{JSON.stringify(state)}</div>
+}
+export const DeleteTask = () => {
+    const [state, setState] = useState<any>(null)
+    useEffect(() => {
+        const todolistId = '76332976-7428-4af5-8ba5-b86e3d1c64af'
+        const taskId = '31da523e-dc84-4b91-96f3-fbd49342f0ff'
+        todolistAPI.deleteTask(todolistId,taskId)
+            .then((res) => {
+                setState(res.data)
+            })
+
+    }, [])
+
+    return <div>{JSON.stringify(state)}</div>
+}
+export const UpdateTaskTitle = () => {
+    const [state, setState] = useState<any>(null)
+    useEffect(() => {
+        const todolistId = '76332976-7428-4af5-8ba5-b86e3d1c64af'
+        const taskId = 'a938fe83-d9af-49e9-9b1b-a1c33a2f331c'
+        const model = {
+            title: "UPDATED-TASK 4",
+            description: null,
+            status: 0,
+            priority: 1,
+            startDate: null,
+            deadline: null,
+        }
+        todolistAPI.updateTask(todolistId, taskId, model)
+            .then((res) => {
+                setState(res.data)
+            })
+    }, [])
+
+    return <div>{JSON.stringify(state)}</div>
+}
