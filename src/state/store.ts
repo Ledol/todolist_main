9f44a96555961ee99c2ce3275b3cf3273187d79f
Dzmitry Liavonenka <dzmitryliavonenka@gmail.com>
2022-10-26 22:58:20 +0300

    feat: - added type for thunk and  AppActionsType

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,7 +1,7 @@
-import { tasksReducer } from './tasks-reducer'
-import { todolistsReducer } from './todolists-reducer'
+import {TasksActionsType, tasksReducer} from './tasks-reducer'
+import {TodolistsActionsType, todolistsReducer} from './todolists-reducer'
 import {applyMiddleware, combineReducers, legacy_createStore} from 'redux'
-import thunk from "redux-thunk";
+import thunk, {ThunkAction} from "redux-thunk";
 
 // объединяя reducer-ы с помощью combineReducers,
 // мы задаём структуру нашего единственного объекта-состояния
@@ -13,6 +13,13 @@ const rootReducer = combineReducers({
 export const store = legacy_createStore(rootReducer, applyMiddleware(thunk))
 // определить автоматически тип всего объекта состояния
 export type AppRootStateType = ReturnType<typeof rootReducer>
+
+//все типы actions для всего app
+export type AppActionsType = TodolistsActionsType | TasksActionsType
+
+// ThunkAction для типизации TC при диспатче action and thunk
+export type AppThunkType<ReturnType = void> = ThunkAction<ReturnType, AppRootStateType, unknown, AppActionsType>
+
 // а это, чтобы можно было в консоли браузера обращаться к store в любой момент
 // @ts-ignore
 window.store = store
