0773bcdd229a7225023ed9cf445a708c0cefa24b
Dzmitry Liavonenka <dzmitryliavonenka@gmail.com>
2022-11-01 23:40:01 +0300

    feat:
    - added setAppErrorAC and new case
    - added new app-reducer test with 2 test case check change error and status

--- src/state/app-reducer.ts
+++ src/state/app-reducer.ts
@@ -5,18 +5,18 @@ const initialState = {
     error: null as null | string
 }
 
-type InitialStateType = typeof initialState
-
 export const appReducer = (state: InitialStateType = initialState, action: ActionsType): InitialStateType => {
     switch (action.type) {
         case 'APP/SET-STATUS':
             return {...state, status: action.status}
+        case "APP/SET-ERROR":
+            return {...state, error: action.error}
         default:
             return state
     }
 }
 
-// action
+// ACTIONS
 
 export type SetAppStatusACType = ReturnType<typeof setAppStatusAC>
 export const setAppStatusAC = (status: RequestStatusType) => {
@@ -26,5 +26,17 @@ return{
 }as const
 }
 
+export type SetAppErrorACType = ReturnType<typeof setAppErrorAC>
+export const setAppErrorAC = (error: null | string) => {
+    return{
+        type: "APP/SET-ERROR",
+        error
+    }as const
+}
 
-type ActionsType = SetAppStatusACType
+// THUNKS
+
+
+// TYPES
+export type InitialStateType = typeof initialState
+type ActionsType = SetAppStatusACType | SetAppErrorACType
